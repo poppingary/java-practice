dae9f1ef8e14710cd2f7386fdfa1b3c726dbd976
Gary <poppingary@gmail.com>
2024-05-29 20:30:01 -0400

    Add Extends Questions example.

---
+++ demo/src/main/java/com/java/practice/extendsquestions/ExtendsQuestionsExample.java
@@ -0,0 +1,24 @@
+package com.java.practice.extendsquestions;
+
+public class ExtendsQuestionsExample {
+    public static void main(String[] args) {
+        Person p = new Student();
+        System.out.println(p.age);
+        p.printAge();
+    }
+}
+
+class Person {
+    public int age = 20;
+    public void printAge() {
+        System.out.println("Person age is: " + age);
+    }
+}
+
+class Student extends Person {
+    public int age = 10;
+
+    public void printAge() {
+        System.out.println("Person age is: " + age);
+    }
+}
