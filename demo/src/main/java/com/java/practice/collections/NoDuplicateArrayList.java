bd6d755505775c6ec9e4eae450e6c6aa77f25241
Gary <poppingary@gmail.com>
2025-01-13 12:32:29 -0500

    Add customized array list code.

---
+++ demo/src/main/java/com/java/practice/collections/NoDuplicateArrayList.java
@@ -0,0 +1,25 @@
+package com.java.practice.collections;
+
+import java.util.ArrayList;
+
+public class NoDuplicateArrayList<E> extends ArrayList<E> {
+    @Override
+    public boolean add(E e) {
+        if (this.contains(e)) {
+            return false;
+        }
+        return super.add(e);
+    }
+
+    public static void main(String[] args) {
+        NoDuplicateArrayList<String> noDuplicateArrayList = new NoDuplicateArrayList<>();
+        noDuplicateArrayList.add("A");
+        noDuplicateArrayList.add("A");
+        noDuplicateArrayList.add("B");
+        noDuplicateArrayList.add("B");
+        noDuplicateArrayList.add("C");
+        noDuplicateArrayList.add("C");
+
+        System.out.println(noDuplicateArrayList);
+    }
+}
