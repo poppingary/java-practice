473e711aac2458465126068e9307de69074b54ad
Gary <poppingary@gmail.com>
2025-05-19 22:40:17 -0700

    Refactor the code.

--- demo/src/main/java/com/java/practice/stream/MapAndFlatMapDemo.java
+++ demo/src/main/java/com/java/practice/stream/MapAndFlatMapDemo.java
@@ -1,14 +1,18 @@
 package com.java.practice.stream;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class MapAndFlatMapDemo {
     public static void main(String[] args) {
-        IntStream streamOfNumbers = IntStream.rangeClosed(1, 5);
-        IntStream squareNumber = streamOfNumbers.map(n -> n * n);
-        squareNumber.forEach(System.out::println); // [1, 4, 9, 16, 25]
+        List<Integer> squareNumber = IntStream.rangeClosed(1, 5)
+                .map(n -> n * n)
+                .boxed()
+                .collect(Collectors.toList());
+        System.out.println(squareNumber); // [1, 4, 9, 16, 25]
 
         Stream<List<Integer>> streamOfLists = Stream.of(
                 List.of(1, 2),
@@ -16,7 +20,9 @@ public class MapAndFlatMapDemo {
                 List.of(6)
         );
 
-        Stream<Integer> flattenedStream = streamOfLists.flatMap(list -> list.stream());
-        flattenedStream.forEach(System.out::println); // [1, 2, 3, 4, 5, 6]
+        List<Integer> flattenedList = streamOfLists
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+        System.out.println(flattenedList); // [1, 2, 3, 4, 5, 6]
     }
 }
